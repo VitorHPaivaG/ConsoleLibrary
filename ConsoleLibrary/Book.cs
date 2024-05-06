0cb6a6df9fde60ee5ddf652488b733d15b4c9492
Vitor Hugo <ovhpgs@gmail.com>
2024-05-06 12:52:40 -0300

    Adicionar arquivos de projeto.

---
+++ ConsoleLibrary/Book.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleLibrary
+{
+    class Book
+    {
+        //static private List<string> bookInfo = new List<string>(); //necessario?
+        static private Dictionary<string, string> bookShelf = new Dictionary<string, string>();    
+
+        public string Title {  get; private set; }//uma abordagem melhor?
+        public string Author { get; private set; }
+        public int Pages { get; private set; }
+
+        public Book() { }
+        public Book(string _title, string _author, int _pages)
+        {
+            Title = _title;
+            Author = _author;
+            Pages = _pages;
+
+            bookShelf.Add(Title, Author);
+            Console.WriteLine("Book added succesfully!");
+        }
+
+
+
+        public string BookDetails()
+        {
+            return $"Title: {Title}\nAuthor: {Author}\nPages: {Pages}";
+        }
+
+
+    }
+}
---
+++ ConsoleLibrary/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleLibrary
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.Title = "Console Library Manager";
+            Console.WriteLine("Welcome to the library");
+
+            Console.WriteLine("What you want to do?\n");
+
+            Book book = new Book("The screwtappes letters", "C.S Lewis", 206);
+
+            Console.WriteLine(book.BookDetails());
+
+
+            EndProgram();
+        }
+
+        static void EndProgram()
+        {
+            Console.WriteLine("");
+            Console.WriteLine("Press ENTER to close the terminal");
+            Console.ReadKey();
+        }
+    }
+}
